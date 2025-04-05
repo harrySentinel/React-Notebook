a382e7df1fcec601e8f525adab702f4c1697e0c0
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-04-05 19:52:48 +0530

    understanding simple useState

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -4,10 +4,16 @@ import './App.css'
 
 function App() {
 
+  const [count, setCount] = useState(0)
+
+ function increaseCount() {
+  setCount(count => count+1)
+ }
 
   return (
     <>
-     
+     <h1>{count}</h1>
+     <button onClick={increaseCount}>Increase Count</button>
     </>
   )
 }
